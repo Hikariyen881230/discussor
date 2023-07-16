f86233d47277dacd69ba80e968b4cdb3f36c431f
Hikariyen881230 <hikariyen881230@gmail.com>
2023-07-16 16:50:46 +0800

    [ complete ]custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,20 @@ module.exports = {
       screens: {
         s: { min: '200px', max: '640px' },
       },
+      colors: {
+        transparent: 'transparent',
+      },
+      backgroundColor: {
+        gradientColor: '#073050',
+        gradientColorHover: '#0a4069',
+        inputColor: '#252b4b',
+      },
+      backgroundImage: {
+        gradientColor:
+          'linear-gradient(213deg, #073050e3 0%, #755a65d3 55%, #313131e1 100%)',
+        gradientColorHover:
+          'linear-gradient(213deg, #0a4069 0%, #92717ed3 55%, #474747e1 100%)',
+      },
     },
   },
   plugins: [],
