9693c6ebd6d47a634f9154e8da056b8445cccc9a
Hikariyen881230 <hikariyen881230@gmail.com>
2023-07-16 16:29:00 +0800

    [ complete ]新增break point

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        s: { min: '200px', max: '640px' },
+      },
+    },
   },
   plugins: [],
 }
