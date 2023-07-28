589d198af819076002db696d51f13d0de7d8ac22
Hikariyen881230 <hikariyen881230@gmail.com>
2023-07-28 15:45:33 +0800

    [ add ]建立schema及model以及Next Auth settings

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,64 @@
+import NextAuth from 'next-auth/next'
+import prisma from '../../../libs/prismaDB'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import GoogleProvider from 'next-auth/providers/google'
+import GithubProvider from 'next-auth/providers/github'
+import bcrypt from 'bcrypt'
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID ?? '',
+      clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
+    }),
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'text' },
+      },
+      async authorize(credentials: any, req) {
+        // 檢查信箱及密碼
+        if (!credentials.email || !credentials.password) {
+          throw new Error('請填寫信箱或密碼!')
+        }
+        // 檢查用戶存在以及是否輸入密碼
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        })
+        if (!user || !user?.hashedPassword) {
+          throw new Error('不存在此用戶!')
+        }
+        // 檢查密碼是否一致
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword
+        )
+        if (!passwordMatch) {
+          throw new Error('請輸入正確的信箱或密碼!')
+        }
+        return user
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  secret: process.env.SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  debug: process.env.NODE_EN === 'development',
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
